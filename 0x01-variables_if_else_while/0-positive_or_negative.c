8b64b71dce5e0ba6b68fc75e04900147f922223e
Olusegun Ekoh <segun.ekoh@gmail.com>
2023-10-12 07:58:53 -0700

    chore(conditionals): `if...else`

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Program generates a random numbers
+*
+* Description: Program generates a random number
+* and prints it as well as its designation as positive,
+* negative or zero.
+*
+* Return: 0 if execution has no errors
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n == 0){
+          printf("%d is zero\n", n);
+	}
+	else if (n > 0){
+          printf("%d is positive\n", n);
+	}
+        else {
+          printf("%d is negative\n", n);
+	}	
+	return (0);
+}
