2ff4cbab04b8b2f3513419ac7b0c08da6fcbc36e
Olusegun Ekoh <segun.ekoh@gmail.com>
2023-10-12 08:21:01 -0700

    fix: Betty `no_new_line` lint error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,13 +21,16 @@ int main(void)
 	{
           printf("%d is zero\n", n);
 	}
+
 	else if (n > 0)
 	{
           printf("%d is positive\n", n);
 	}
+
         else 
 	{
           printf("%d is negative\n", n);
-	}	
+	}
+
 	return (0);
 }
